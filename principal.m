f3994dfb0016785464c4cb51ae1a24687462b45d
AntonioMateus <antoniocms1995@hotmail.com>
2015-07-02 11:54:44 -0300

    Update principal.m

--- principal.m
+++ principal.m
@@ -1,14 +1,19 @@
-function [] = principal (treinamento, teste, lag)   
-    [X,Y,c,min,max] = monta_matrizes(treinamento, teste, lag); 
-    %[Xn, Yn] = normalizacao_dados(X,Y,lag);
-    [X_trein, ~, Y_trein, ~] = separa_conjuntos(X, Y, lag, c); 
-    constroi_fuzzy(X_trein,Y_trein,lag,treinamento(17),min,max);
-%     fismat = readfis('teste_rede.fis');
-%     saida = evalfis(X_trein, fismat);
-%     erro = Y_trein - saida; 
-%     et = 0; 
-%     for i = 1:length(erro)
-%         et = et + (erro(i,1)^2); 
-%     end
-%     eqm = et/length(Y_trein);
+function [] = principal (treinamento, teste, lag, normaliza, num_epocas)   
+    [X,Y,c,~,~] = monta_matrizes(treinamento, teste, lag); 
+    [X_trein, X_teste, Y_trein, Y_teste] = separa_conjuntos(X, Y, lag, c);
+    if (normaliza==1) 
+        [X_trein, Y_trein] = normalizacao_dados(X_trein,Y_trein,lag);
+        [X_teste, Y_teste] = normalizacao_dados(X_teste,Y_teste,lag);
+        s = 'com';
+    else 
+        s = 'sem';
+    end         
+    [fuzzy_sugeno, erro_treinamento] = constroi_fuzzy(X_trein,Y_trein,treinamento(17),num_epocas);
+    saida = evalfis(X_teste, fuzzy_sugeno);
+    erro_teste = Y_teste - saida; 
+    et = 0; 
+    for i = 1:length(erro_teste)
+        et = et + (erro_teste(i,1)^2); 
+    end
+    fprintf ('(%s normalizacao) O erro quadratico total eh de: %1.4f (para teste) e %1.4f (para treinamento)\n', s, et,erro_treinamento);
 end
