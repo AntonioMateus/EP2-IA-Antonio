e218337266d47dea8c405f4c8dcc02878f319622
AntonioMateus <antoniocms1995@hotmail.com>
2015-07-03 23:37:31 -0300

    Update constroi_fuzzy.m

--- constroi_fuzzy.m
+++ constroi_fuzzy.m
@@ -1,4 +1,4 @@
-function [fuzzy_sugeno_otimo, erro_otimo] = constroi_fuzzy(X, Y, indice, num_epocas)
+function [fuzzy_sugeno_otimo, erro_otimo] = constroi_fuzzy(X_trein, Y_trein, X_teste, Y_teste, indice, num_epocas, lag, modo)
     %nesse EP, sera utilizado o metodo grid partition para a criação de
     %estruturas sugeno 
     tipos_imf = cell(8);
@@ -13,26 +13,56 @@ function [fuzzy_sugeno_otimo, erro_otimo] = constroi_fuzzy(X, Y, indice, num_epo
     tipos_omf = cell(2); 
     tipos_omf{1} = 'linear'; 
     tipos_omf{2} = 'constant'; 
-    entrada = [X Y];
-    pos = 1; 
-    matriz = zeros((length(tipos_imf)*length(tipos_omf)),2);
-    for i = 1:length(tipos_imf)
-        for j = 1:length(tipos_omf)
-            f = genfis1(entrada,4,tipos_imf{i},tipos_omf{j}); 
-            estruturas_fuzzy(pos) = anfis(entrada, f, [num_epocas 0.05 0.01 0.9 1.1],[0 0 0 0]); 
-            matriz(pos,2) = pos;
-            saida = evalfis(X,estruturas_fuzzy(pos));
-            erro = Y - saida; 
-            et = 0; 
-            for ind = 1:length(erro)
-                et = et+(erro(ind,1)^2); 
-            end
-            matriz(pos,1) = et; 
-            pos = pos + 1; 
-        end 
+    treinamento = [X_trein Y_trein];
+    teste = [X_teste Y_teste];
+    limiar_erro = sqrt(0.05); 
+    m = 1; %default
+    if strcmp(modo,'back-propagation')
+        m = 0; 
+    else 
+        m = 1; 
     end
-    matriz_ordenada = sortrows(matriz); 
-    fuzzy_sugeno_otimo = estruturas_fuzzy(matriz_ordenada(1,2)); 
-    erro_otimo = matriz_ordenada(1,1); 
+    if lag <= 2
+        pos = 1; 
+        matriz = zeros((length(tipos_imf)*length(tipos_omf)),2);
+        for i = 1:length(tipos_imf)
+            for j = 1:length(tipos_omf)
+                f = genfis1(treinamento,4,tipos_imf{i},tipos_omf{j}); 
+                [~,~,~,estruturas_fuzzy(pos),~] = anfis(treinamento, f, [num_epocas limiar_erro 0.01 0.9 1.1],[0 0 0 0],teste,m); 
+                matriz(pos,2) = pos;
+                saida = evalfis(X_trein,estruturas_fuzzy(pos));
+                erro = Y_trein - saida; 
+                et = 0; 
+                for ind = 1:length(erro)
+                    et = et+(erro(ind,1)^2); 
+                end
+                matriz(pos,1) = et; 
+                pos = pos + 1; 
+            end 
+        end
+        matriz_ordenada = sortrows(matriz); 
+        fuzzy_sugeno_otimo = estruturas_fuzzy(matriz_ordenada(1,2));
+    else
+        radii = zeros(1,2+lag); 
+        for i=1:(1+lag)
+            objeto = evalclusters(X_trein(:,i),'kmeans','Silhouette','KList',1:10);
+            num_clusters_ideal = objeto.OptimalK;
+            clusters = kmeans(X_trein(:,i),num_clusters_ideal);
+            radii(1,i) = devolve_vizinhanca(X_trein(:,i),clusters,num_clusters_ideal);
+        end    
+        objeto = evalclusters(Y_trein,'kmeans','Silhouette','kList',1:10);
+        num_clusters_ideal = objeto.OptimalK; 
+        clusters = kmeans(Y_trein,num_clusters_ideal);
+        radii(1,(2+lag)) = devolve_vizinhanca(Y_trein,clusters,num_clusters_ideal);
+        fismat = genfis2(X_trein,Y_trein,radii); 
+        [~,~,~,fuzzy_sugeno_otimo,~] = anfis(treinamento, fismat, [num_epocas limiar_erro 0.01 0.9 1.1],[0 0 0 0],teste,m);    
+    end
+    %fuzzy_mandami_otimo = sug2mam(fuzzy_sugeno_otimo);
+    saida = evalfis(X_trein,fuzzy_sugeno_otimo);
+    erro = Y_trein - saida; 
+    erro_otimo = 0; 
+    for ind = 1:length(erro)
+        erro_otimo = erro_otimo+(erro(ind,1)^2); 
+    end
     writefis(fuzzy_sugeno_otimo,strcat('fuzzy',indice,'.fis')); 
 end 
