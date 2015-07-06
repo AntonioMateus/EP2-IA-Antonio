83fe35838932929098505c84b2da41a488f0cd93
AndreTGMello <andre.tgmello@gmail.com>
2015-07-06 16:17:36 -0300

    Update principal.m

--- principal.m
+++ principal.m
@@ -1,13 +1,10 @@
 function [erro_treinamento,et] = principal (treinamento, teste, lag, normaliza, num_epocas, intervalo)   
-% RETIRAR INTERVALO!!!!
+
 [X,Y,c,~,~] = monta_matrizes(treinamento, teste, lag); 
     [X_trein, X_teste, Y_trein, Y_teste] = separa_conjuntos(X, Y, lag, c);
     if (normaliza==1) 
         [X_trein, Y_trein] = normalizacao_dados(X_trein,Y_trein,lag);
         [X_teste, Y_teste] = normalizacao_dados(X_teste,Y_teste,lag);
-%         s = 'com';
-%     else 
-%         s = 'sem';
     end        
     [fuzzy_sugeno, fuzzy_mamdani, erro_treinamento] = constroi_fuzzy(X_trein,Y_trein,X_teste,Y_teste,treinamento(6),num_epocas, lag, intervalo);
     saida_teste_mam = evalfis(X_teste, fuzzy_mamdani);
